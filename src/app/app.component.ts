46c72237c485f3af01ac6fcd3237885df5d26c9d
Faith Schneider <niahcx@yahoo.com>
2017-10-16 17:00:43 -0600

    ng-for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class Hero {
   selector: 'my-app',
 	// link to the HTML template
   templateUrl: './app.component.html',
-	
 })
-export class AppComponent  { 
+
+export class AppComponent  {
 	title = 'Angular';
 	
 	hero: Hero = {
@@ -20,3 +20,18 @@ export class AppComponent  {
 	};
 	
 }
+
+const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice' },
+	{ id: 12, name: 'Narco' },
+	{ id: 13, name: 'Bombasto' },
+	{ id: 14, name: 'Celeritas' },
+	{ id: 15, name: 'Magneta' },
+	{ id: 16, name: 'RubberMan' },
+	{ id: 17, name: 'Dynama' },
+	{ id: 18, name: 'Dr IQ' },
+	{ id: 19, name: 'Magma' },
+	{ id: 20, name: 'Tornado' }
+];
+
+heroes = HEROES;
